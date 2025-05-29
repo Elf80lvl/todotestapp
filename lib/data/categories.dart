5f0a5c2e9d52fc7e557aae03036bc9ad9067a1df
Elf80lvl <a.blake.mail@gmail.com>
2025-05-29 14:55:32 +0300

    2

--- lib/data/categories.dart
+++ lib/data/categories.dart
@@ -1,17 +1,14 @@
-class CategoryManager {
-  final List<String> _categories = [];
+class Categories {
+  final List<String> _categories = ['category1', 'category2'];
 
-  // Получить все категории
   List<String> get categories => List.unmodifiable(_categories);
 
-  // Добавить новую категорию
   void addCategory(String category) {
     if (!_categories.contains(category)) {
       _categories.add(category);
     }
   }
 
-  // Удалить категорию
   void removeCategory(String category) {
     _categories.remove(category);
   }
